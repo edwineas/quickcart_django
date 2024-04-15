c44d8e91a258b239a08bf57b65c636793213959a
edwineas <edwinalexshaji@gmail.com>
2024-04-15 22:41:34 +0530

    redirection

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,7 @@ from .models import Customer, Shopkeeper
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 
+@permission_classes([AllowAny])
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
