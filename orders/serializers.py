01991ee1c139e7d5ca70cef40ad95bced4161765
edwineas <edwinalexshaji@gmail.com>
2024-04-23 15:05:22 +0530

    shop orderview

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,25 +3,30 @@ from rest_framework import serializers
 from .models import Order, OrderShop, OrderProduct
 from users.models import Customer
 
+
+
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id','user', 'first_name', 'last_name', 'email', 'phone_number']
-        
-class OrderProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderProduct
-        fields = ['order_shop', 'product','product_name', 'product_price', 'product_quantity']
-
-class OrderShopSerializer(serializers.ModelSerializer):
-    products = OrderProductSerializer(many=True, read_only=True)
-    class Meta:
-        model = OrderShop
-        fields = ['order', 'shop','shop_name', 'shop_price', 'packet_picked', 'payment_received', 'products']
+        fields = ['first_name', 'last_name']
 
 class OrderSerializer(serializers.ModelSerializer):
-    shops = OrderShopSerializer(many=True, read_only=True)
     customer = CustomerSerializer(read_only=True)
     class Meta:
         model = Order
-        fields = ['id', 'date', 'customer', 'price', 'status', 'shops']
+        fields = ['id', 'date', 'customer']
+
+class OrderShopSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only=True)
+
+    class Meta:
+        model = OrderShop
+        fields = ['order', 'shop', 'shop_name', 'shop_price', 'packet_picked', 'payment_received']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        order_representation = representation.pop('order')
+        representation['order'] = order_representation['id']
+        representation['date'] = order_representation['date']
+        representation['customer_name'] = f"{order_representation['customer']['first_name']} {order_representation['customer']['last_name']}"
+        return representation
--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import OrderShop
-from .serializers import OrderSerializer
+from .serializers import OrderShopSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 
 @permission_classes([AllowAny])
 class OrderShopView(APIView):
     def get(self, request, shop_id):
-        ordershop = OrderShop.objects.filter(shop_id=shop_id)
-        serializer = OrderSerializer(ordershop, many=True)
+        ordershop = OrderShop.objects.filter(shop__id__exact=shop_id)
+        serializer = OrderShopSerializer(ordershop, many=True)
         return Response(serializer.data)
