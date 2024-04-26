2ca6b3f77d1a515834b88f9cd1904eb3adf718db
edwineas <edwinalexshaji@gmail.com>
2024-04-26 11:01:33 +0530

    database changed and vercel

--- quickcart/settings.py
+++ quickcart/settings.py
@@ -99,10 +99,21 @@ WSGI_APPLICATION = 'quickcart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.ffdawaoqehifvpsqxsvj",
+        "PASSWORD": "quickcart@2024",
+        "HOST": "aws-0-us-west-1.pooler.supabase.com",
+        "PORT": "5432",
     }
 }
 
