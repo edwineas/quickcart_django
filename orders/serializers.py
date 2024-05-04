bd7332468b0522724121549077696fb1e6e72a75
edwineas <edwinalexshaji@gmail.com>
2024-05-04 10:21:56 +0530

    cleanned up

--- orders/serializers.py
+++ orders/serializers.py
@@ -16,30 +16,9 @@ class ProductSerializer(serializers.Serializer):
     quantity = serializers.IntegerField()
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
 
-class OrderSerializer(serializers.Serializer):
+class MappingSerializer(serializers.Serializer):
     shop = ShopsSerializer()
     shop_name = serializers.CharField()
     shop_rating = serializers.FloatField()
     products = ProductSerializer(many=True)
     total_price = serializers.DecimalField(max_digits=10, decimal_places=2)
-
-class OrderShopSerializer(serializers.ModelSerializer):
-    order = OrderSerializer(read_only=True)
-
-    class Meta:
-        model = OrderShop
-        fields = ['id','order', 'shop', 'shop_name', 'shop_price', 'packet_picked', 'payment_received']
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        order_representation = representation.pop('order')
-        representation['order'] = order_representation['id']
-        representation['date'] = order_representation['date']
-        representation['customer_name'] = f"{order_representation['customer']['first_name']} {order_representation['customer']['last_name']}"
-        representation['customer_id'] = order_representation['customer']['id']
-        return representation
-
-class OrderProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderProduct
-        fields = ['product_name', 'product_price', 'product_quantity']
--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import OrderShop, OrderProduct
 from shops.models import Shops, Inventory
-from .serializers import OrderShopSerializer, OrderProductSerializer, OrderSerializer
+from .serializers import MappingSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 import googlemaps
@@ -12,22 +12,6 @@ import os
 from dotenv import load_dotenv
 
 
-
-
-@permission_classes([AllowAny])
-class OrderShopView(APIView):
-    def get(self, request, shop_id):
-        ordershop = OrderShop.objects.filter(shop__id__exact=shop_id)
-        serializer = OrderShopSerializer(ordershop, many=True)
-        return Response(serializer.data)
-
-@permission_classes([AllowAny])
-class OrderProductView(APIView):
-    def get(self, request, order_shop_id):
-        order_products = OrderProduct.objects.filter(order_shop_id=order_shop_id)
-        serializer = OrderProductSerializer(order_products, many=True)
-        return Response(serializer.data)
-    
 @permission_classes([AllowAny])
 class OrderMappingView(APIView):
     def post(self, request):
@@ -101,6 +85,6 @@ class OrderMappingView(APIView):
                     current_location = (shop.latitude, shop.longitude)
                     break
         # Serialize and return response
-        serializer = OrderSerializer(selected_shops, many=True)
+        serializer = MappingSerializer(selected_shops, many=True)
         print(serializer.data)
         return Response(serializer.data)
