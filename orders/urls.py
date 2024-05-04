794860f5cdae0f68c798bf4540dfb9fb4c7100d2
edwineas <edwinalexshaji@gmail.com>
2024-05-04 10:27:25 +0530

    error

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 # order.urls.py
 from django.urls import path
-from .views import OrderShopView, OrderProductView, OrderMappingView
+from .views import OrderMappingView
 
 urlpatterns = [
-    path('shop/<int:shop_id>/', OrderShopView.as_view(), name='shop_orders'),
-    path('products/<int:order_shop_id>/', OrderProductView.as_view(), name='order_products'),
+    # path('shop/<int:shop_id>/', OrderShopView.as_view(), name='shop_orders'),
+    # path('products/<int:order_shop_id>/', OrderProductView.as_view(), name='order_products'),
     path('mapping/', OrderMappingView.as_view(), name='order_mapping'),
 ]
